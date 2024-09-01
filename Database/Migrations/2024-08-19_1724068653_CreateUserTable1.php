0621a58ffbd5ae25629f34c4dc7d804e4edc6de0
kashmii <www.myac@gmail.com>
2024-09-01 19:45:16 +0900

    不要だったuserテーブルのカラム削除

--- Database/Migrations/2024-08-19_1724068653_CreateUserTable1.php
+++ Database/Migrations/2024-08-19_1724068653_CreateUserTable1.php
@@ -14,7 +14,6 @@ class CreateUserTable1 implements SchemaMigration
         username VARCHAR(255) NOT NULL,
         email VARCHAR(255) NOT NULL UNIQUE,
         password VARCHAR(255) NOT NULL,
-        email_confirmed_at VARCHAR(255),
         created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
         updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
       )"
